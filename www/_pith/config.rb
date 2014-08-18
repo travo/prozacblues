2c2556e524b95582a9cee4f0b3d8e912f52c1129
Travis Winters <traviswinters@gmail.com>
2014-08-18 22:53:44 +1000

    Tear the stupid stuff out of config.rb

--- www/_pith/config.rb
+++ www/_pith/config.rb
@@ -8,23 +8,46 @@ project.assume_directory_index = false
 
 project.helpers do
 
+  def input_for_index(topic = nil)
+    if topic
+      posts = posts_by_topic(topic)
+      sorted(posts).reverse
+    else
+      path = File.dirname(page.file)
+      posts = posts_by_path(path)
+      sorted(posts)
+    end
+  end
+
+  def posts_by_path(path)
+    project.inputs.select do |input|
+      next  if input.ignorable? || index?(input.file.to_s)
+      input if input.file.to_s.match(path)
+    end
+  end
+
+  def posts_by_topic(topic)
+    project.inputs.select do |input|
+      next  if input.ignorable? || index?(input.file.to_s) || input.meta['topics'].nil?
+      input if input.meta['topics'].include?(topic)
+    end
+  end
+
+  def sorted(posts)
+    posts.sort do |newest, oldest|
+      newest.meta['post_date'] <=> oldest.meta['post_date']
+    end
+  end
+
   def main_header_image
     if page.meta['main_image']
       %{<style type="text/css"> header { background-image: url(#{href(page.meta['main_image'])}) } </style>}
     end
   end
 
-  def blog_meta
-    post_date = DateTime.parse(page.meta['post_date'].to_s)
-    formatted_date = post_date.strftime("%e#{post_date.day.ordinal} %B %Y")
-    %{
-      #{topic_links}
-      <p>
-        <small><strong>Posted</strong></small>
-        <br/>
-        #{formatted_date}
-      </p>
-    }
+  def formatted_date(date)
+    post_date = DateTime.parse(date.to_s)
+    post_date.strftime("%e#{post_date.day.ordinal} %B %Y")
   end
 
   def topic_links
@@ -33,65 +56,10 @@ project.helpers do
     end.join(' ')
   end
 
-  def local_index(indexfile)
-    relpath  = indexfile.split('/')[0..-2]
-    filespec = [Dir.getwd, relpath, '*.html.haml'].flatten
-    filepath = File.join(filespec)
-    Dir.glob(filepath).map do |file|
-      post_link(file) unless index?(file)
-    end.join("\n\n")
-  end
-
-  def topic_index(topic)
-    pathspec = File.join(Dir.getwd, 'www', 'blog', '**/*.html.haml')
-    Dir.glob(pathspec).reverse.map do |file|
-      meta = read_meta(File.open(file))
-      if !index?(file) && meta['topics']
-        topics = meta['topics'].split(', ')
-        post_link(file, meta) if topics.include?(topic)
-      end
-    end.join("\n\n")
-  end
-
-  def post_link(file, pagemeta = nil)
-    href = file.match(/blog.*\.html/).to_s
-    meta = pagemeta || read_meta(File.open(file))
-    post_date = DateTime.parse(meta['post_date'].to_s)
-    formatted_date = post_date.strftime("%e#{post_date.day.ordinal} %B %Y")
-    %{
-      <h3><a href='/#{href}'>#{meta['title']}</a></h3>
-      <p class='index-post-date'>
-        <small><strong>Posted:</strong> #{formatted_date}</small>
-      </p>
-    }
-  end
-
-
   private
 
   def index?(file)
     file.match(/index\.html\.haml$/) || file.match(/archives\.html\.haml$/)
   end
-
-  YamlParseError = defined?(Psych::SyntaxError) ? Psych::SyntaxError : ArgumentError
-
-  # Ah, for now — stolen right out of pith.
-  def read_meta(io)
-    header = io.gets
-    if header =~ /^---/
-      while line = io.gets
-        break if line =~ /^(---|\.\.\.)/
-        header << line
-      end
-      begin
-        @meta = YAML.load(header)
-      rescue YamlParseError => e
-        puts "#{e}:1: badly-formed YAML header"
-      end
-    else
-      io.rewind
-    end
-  end
-
 
 end
